191f6d09333ab51917da3d489d7bc65c8e6d0f07
Sondar4 <rapega12@gmail.com>
2020-11-13 18:12:38 +0100

    Competadas páginas de mangas

--- display_block.js
+++ display_block.js
@@ -1,5 +1,11 @@
+console.log('Script loaded!')
+
 function display(elem) {
   if ('oculto' in elem.classList) {
     elem.classList.add('titulo-mostrado')
   }
+}
+
+function myTest() {
+  console.log("The code is working!")
 }
